9dcdfc9cc41ae76c0dc98c91ed70ca9f4dd3e4d5
21800349_백승현 <bsj2776@naver.com>
2021-09-05 22:21:01 +0900

    Commit

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,17 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hello World!!!\n");
+		String name = s.next();
+		System.out.println("[" + name + "]님 환영합니다~");
+		
+		
+	}
+
+}
